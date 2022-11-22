225fd381e0f11bec7e52ff9ca43e29ca32794448
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-11-22 13:18:46 +0545

    adding nvm dir

---
+++ install.sh
@@ -0,0 +1,27 @@
+# install nix package manager 
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+# source nix 
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages from nix 
+nix-env -iA \
+    nixpkgs.zsh \
+    nixpkgs.antibody \
+    nixpkgs.git \
+    nixpkgs.stow \
+    nixpkgs.yarn \
+    nixpkgs.nvm \
+
+# stow dotfiles
+stow zsh
+stow nvm
+
+# add zsh to valid login shells 
+command -v zsh | sudo tee -a /etc/shells
+
+# add zsh as default shell 
+sudo chsh -s $(which zsh) $USER
+
+# bundle zsh plugins 
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
