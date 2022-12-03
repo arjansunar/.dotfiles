4cd467054f61410afecda8d0d085d9251d094abe
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-12-03 07:35:15 +0545

    updating aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,10 @@ set -U fish_greeting "🐟"
 
 # aliases 
 alias ls="exa -l -g --icons"
+# open vpn aliases
+alias vpn="openvpn3"
+alias vpn-start="openvpn3 session-start --config $HOME/Documents/vpn-settings/arjan.ovpn"
+alias vpn-close="openvpn3 session-manage -D --config $HOME/Documents/vpn-settings/arjan.ovpn"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
