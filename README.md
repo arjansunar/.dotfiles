f77a84d1c37aa165f1e1ff08b7f609eee4700907
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-12-20 21:22:49 +0545

    removing unused folders and adding readme

---
+++ README.md
@@ -0,0 +1,42 @@
+## Get running in few steps 🚀
+
+### Steps
+
+- update the apt repo
+
+  ```bash
+  sudo apt-get update && apt upgrade
+  ```
+
+- download git
+  ```bash
+  sudo apt-get install git
+  ```
+- clone repo
+
+  ```bash
+  git clone git@github.com:arjansunar/.dotfiles.git
+    # or
+  git clone https://github.com/arjansunar/.dotfiles
+  ```
+
+- cd to the project
+
+  ```bash
+  cd .dotfiles
+  ```
+
+- run install.sh
+  ```bash
+  ./install.sh
+  ```
+
+### Steps after install.sh
+
+1. Install [alacrity](https://github.com/alacritty/alacritty/blob/master/INSTALL.md) and run
+
+   ```bash
+   stow alacrity # from .dotfiles directory
+   ```
+
+2. Install [neovim v8.0](https://github.com/neovim/neovim/releases/tag/v0.8.0) and [astrovim](https://astronvim.github.io/)
--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 # install nix package manager 
-# curl -L https://nixos.org/nix/install | sh
+curl -L https://nixos.org/nix/install | sh
 
 # # source nix 
-# . ~/.nix-profile/etc/profile.d/nix.sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 # install packages from nix 
 nix-env -iA \
-    nixpkgs.fish \ 
-    nixpkgs.git \
-    nixpkgs.stow \
-    nixpkgs.yarn \
-    nixpkgs.neovim \
-    nixpkgs.tmux \
-    nix-env -iA nixpkgs.exa \
+    nixpkgs.fish \ # shell
+    nixpkgs.stow \ # stowing packages
+    nixpkgs.yarn \ # node package builder
+    nixpkgs.tmux \ # terminal multiplexer
+    nixpkgs.exa \  # better ls
+    nixpkgs.bat \  # better cat
+    nixpkgs.gdu \  # file explorer in terminal
+    nixpkgs.lazygit \ # git actions from terminal
 
     # nixpkgs.zsh \
 
 # stow dotfiles
-stow zsh
-stow nvm
 stow tmux
+stow fish
 
 # add fish to valid login shells 
 command -v fish | sudo tee -a /etc/shells
@@ -30,3 +30,4 @@ sudo chsh -s $(which fish) $USER
 # install fisher (package manager)
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
+# install neovim 8.0 and astrovim 
