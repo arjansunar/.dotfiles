74e24ff52e3cf1190d774e572d441b1d0e823aa5
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-03-18 06:42:37 +0545

    updating fish configs and copy to clipboard commands for tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,19 +3,38 @@ set -U fish_greeting ""
 
 # aliases 
 alias ls="exa -l -g --icons"
+alias cl="clear"
+
 # open vpn aliases
 alias vpn="openvpn3"
 alias vpn-s="openvpn3 session-start --config $HOME/Documents/work/vpn-settings/arjan.ovpn"
 alias vpn-c="openvpn3 session-manage -D --config $HOME/Documents/work/vpn-settings/arjan.ovpn"
 
-# deno 
-# export DENO_INSTALL="~/.deno"
-# export PATH="$DENO_INSTALL/bin:$PATH"
+# nvim alias
+alias vim="nvim"
+alias nv="nvim"
+alias vi="nvim"
+# open nvim 
+alias ncd="nvim ." # in current directory
+
+# lazygit alias 
+alias gg="lazygit"
+
+# work
+alias dms-s="z main && TAG=dev make dev"
+alias dms-c="z main && TAG=dev make stop"
+alias dms-r="z main && dms-c && dms-s"
 
 # cargo 
 export PATH="$HOME/.cargo/bin:$PATH"
+
+# nvim using bob nvim version manager
+export PATH="$HOME/.local/share/neovim/bin:$PATH"
 
 # pnpm
 set -gx PNPM_HOME "/home/rjan/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
+
+# Created by `pipx` on 2022-12-30 02:23:56
+set PATH $PATH /home/rjan/.local/bin
