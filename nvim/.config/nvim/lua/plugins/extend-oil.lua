9c32e937a36d8cedd641231be8acd8d7344c09e0
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-01-09 12:36:38 +0545

    feat: use neotree instead of oil

--- nvim/.config/nvim/lua/plugins/extend-oil.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opts = function()
-    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end,
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-}
